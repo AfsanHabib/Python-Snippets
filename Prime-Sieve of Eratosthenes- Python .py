23b22d3d987d2ad4fed254ec7e7452fdc2d4279e
Afsan Habib <85637295+AfsanHabib@users.noreply.github.com>
2022-02-17 21:16:24 +0600

    Update Prime-Sieve of Eratosthenes- Python .py

--- Prime-Sieve of Eratosthenes- Python .py
+++ Prime-Sieve of Eratosthenes- Python .py
@@ -23,3 +23,35 @@ while t:
     genprimes(n)
     print()
     t=t-1
+    
+
+    
+########################################################################
+   
+'''
+#Python Sieve of Eratosthenes
+
+from math import *
+
+def genprimes(n):
+    primes = [True]*(n+1)
+    primes[0] = False
+    primes[1] = False
+    for p in range(2,int(sqrt(n))+1):
+        if primes[p] == True:
+            for i in range(p*p,n+1,p):
+                primes[i] = False
+
+
+            
+    if primes[n] == True:
+    	return False
+    else:
+    	return True
+
+t = int(input())
+while t:
+    n = int(input())
+    print(genprimes(n))
+    print()
+    t=t-1
