bfef6a2e7907bae47ae16636091501ee07512241
Afsan Habib <85637295+AfsanHabib@users.noreply.github.com>
2022-02-17 21:46:03 +0600

    Update Prime-Sieve of Eratosthenes- Python .py

--- Prime-Sieve of Eratosthenes- Python .py
+++ Prime-Sieve of Eratosthenes- Python .py
@@ -1,5 +1,37 @@
 #Python Sieve of Eratosthenes
 
+import math
+def getPrimes():
+    n = 10**6+1
+    seive = [0]*(n+1)
+    # 0 and 1 are not primes
+    seive[0] = 1
+    seive[1] = 1
+ 
+    for i in range(2, int(math.sqrt(n))+1):
+        if seive[i] == 0:
+            for j in range(i, n+1):
+                if j*i>n:
+                    break
+                else:
+                    if seive[i*j]==0:
+                        seive[i*j] = 1
+    return seive
+
+
+primes=getPrimes()
+n=17
+
+if primes[n]==0:
+	print("Prime")
+else:
+	print("Not Prime")
+
+
+   
+
+#######################################
+'''
 from math import *
 
 def genprimes(n):
@@ -25,7 +57,7 @@ while t:
     t=t-1
     
 
-    
+'''
 ########################################################################
    
 '''
